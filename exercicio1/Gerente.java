cd9b011b01538a43b7e2b3598a07c4924a612b36
Pedro-Leles <pedro.alvim@soulasalle.com.br>
2025-06-05 18:44:24 -0300

    Create Gerente.java

---
+++ exercicio1/Gerente.java
@@ -0,0 +1,10 @@
+public class Gerente extends Funcionario {
+    public Gerente(String nome, double salarioBase) {
+        super(nome, salarioBase);
+    }
+
+    @Override
+    public double calcularSalario() {
+        return salarioBase * 1.20;
+    }
+}
